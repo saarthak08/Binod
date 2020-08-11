6fba9940e847ac3bd23065086b285c49230b0c92
Saarthak Gupta <saarthakgupta08@gmail.com>
2020-08-11 09:15:22 +0530

    Binod

--- Binod.cs
+++ Binod.cs
@@ -1,6 +1,6 @@
-namespace HelloWorld
+namespace Binod
 {
-    class Hello {         
+    class Binod {         
         static void Main(string[] args)
         {
             System.Console.WriteLine("Binod");
